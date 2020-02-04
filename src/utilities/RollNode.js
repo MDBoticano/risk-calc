98eb3813e900b378b0a0e0c07b65146ac3cee425
mdboticano <mdboticano@gmail.com>
2020-02-04 15:29:41 -0800

    Fix: return parentNode outside for loop

--- src/utilities/RollNode.js
+++ src/utilities/RollNode.js
@@ -1,7 +1,20 @@
-import {
-  loseTroops,
-} from './ProbTree';
+export const loseTroops = (attackers, defenders, troopsLost = 2) => {
+  const outcomes = [
+    [attackers - troopsLost, defenders],
+    [attackers, defenders - troopsLost],
+  ];
 
+  if (troopsLost === 2) { outcomes.push([attackers - 1, defenders - 1]) };
+
+  // TODO: sort outcomes in a way to easily traverse the resulting tree
+
+  return outcomes;
+}
+
+/**
+ * RollNode object to create tree
+ * @param {[Number]} rollPair -- array of two numbers: [nAtk, nDef]
+ */
 export const RollNode = class {
   constructor(rollPair) {
     this.parent = null;
@@ -12,44 +25,53 @@ export const RollNode = class {
   // Methods //
   // set parent (parentNode) { this.parent = parentNode };
 
-  addChild (childNode) { this.children.push(childNode) };
+  addChild (childNode) { this.children.push(childNode); };
 
-  get nAttackers () { return this.rollPair[0] };
-  get nDefenders () { return this.rollPair[1] };
+  get nAttackers () { return this.rollPair[0]; };
+  get nDefenders () { return this.rollPair[1]; };
 
-  // calculate probabilities: child to parent
+  // TODO: calculate probabilities: child to parent
   // Q: calculate from within or pass as a calculated value when creating node? 
   // Or maybe make it part of loseTroops. So rollPair will be three values:
   // 1. total number of attackers (can be greater than 3)
   // 2. total number of defenders (can be greater than 2)
   // 3. probability relative to parent
+
+  // TODO: calculate depth (levels of children). no children is depth 1.
+  
+  // TODO: calculate total number of nodes 
 }
 
+/**
+ * Create a tree of all possible outcomes
+ * @param {RollNode} parentNode - node to attach children to
+ */
 export const createTree = (parentNode) => {
-  if (!(parentNode instanceof RollNode)) return;
+  // Check that the parameter is a node
+  if (!(parentNode instanceof RollNode)) { return; };
 
-  // check values for zero
+  // Base case: if the node's pair has any zeroes, it cannot have kids
   const [nodeAtk, nodeDef] = [ parentNode.nAttackers, parentNode.nDefenders ];
-  if (nodeAtk <= 0 || nodeDef <= 0) return parentNode;
+  if (nodeAtk === 0 || nodeDef === 0) { return parentNode };
 
-  let children;  
   // otherwise, create the tree
+  let children;  
   if (nodeAtk >= 2 && nodeDef >= 2) {
     children = loseTroops(nodeAtk, nodeDef, 2);
   } else {
     children = loseTroops(nodeAtk, nodeDef, 1);
   }
-
+  // console.log(children, children.length);
   for (let i = 0; i < children.length; i++) {
     // turn child into a node
     const newNode = new RollNode(children[i]);
-    newNode.parent = parentNode;
+    newNode.parent = parentNode; // attach the child to the parent
 
-    // CREATE tree from children
-    const childTree = createTree(newNode);
+    // recursive case: CREATE tree from children
+    const childTree = createTree(newNode); 
 
+    // add that node to the parent node
     parentNode.addChild(childTree);
-    return parentNode;
   }
-
+  return parentNode;
 }
--- src/utilities/RollNode.test.js
+++ src/utilities/RollNode.test.js
@@ -9,16 +9,25 @@ describe('RollNode class', () => {
   //   console.log(newNode);
   // });
 
-  // test('create a tree from a terminating node', () => {
-  //   const terminatingNode = new RollNode([1, 0]);
-  //   const newTree = createTree(terminatingNode);
-  //   console.log(newTree);
-  // });
+  test('create a tree from a terminating node', () => {
+    const terminatingNode = new RollNode([1, 0]);
+    const newTree = createTree(terminatingNode);
+    console.log(newTree);
+  });
 
   test('create a tree from a non-terminating node', () => {
     const nonTerminatingNode = new RollNode([1, 1]);
     const newTree = createTree(nonTerminatingNode);
-    // console.log(newTree);
-    console.log(newTree.children[0]);
+    console.log(newTree);
+    expect((newTree.children).length).toBe(2);
+  });
+
+  test('create a tree with depth 3', () => {
+    const newNode = new RollNode([2,2]);
+    const newTree = createTree(newNode);
+    console.log(newTree);
+    expect(newTree.children.length).toBe(3);
+    const thirdChild = newTree.children[2];
+    expect(thirdChild.children.length).toBe(2);
   });
 });
