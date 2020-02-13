e81223b6642180fe0c8451a8153e9fe49ec6d1f5
mdboticano <mdboticano@gmail.com>
2020-02-12 17:30:10 -0800

    WIP: Style calculation form

--- src/App.js
+++ src/App.js
@@ -6,43 +6,29 @@ import {
 } from './utilities/RollNode';
 
 const App = () => {
-  const [numAttackers, setNumAttackers] = useState(0);
-  const [numDefenders, setNumDefenders] = useState(0);
-  const [modifiers] = useState(); // not yet used
+  const [numAttackers, setNumAttackers] = useState("");
+  const [numDefenders, setNumDefenders] = useState("");
+  const [modifiers, setModifiers] = useState({ defender: "" });
 
   const [oddsAttackWins, setOddsAttackWins] = useState([]);
   const [oddsAttackLoses, setOddsAttackLoses] = useState([]);
-
-  // return outcomes percentages in a table
 
   const calculateOdds = (event) => {
     event.preventDefault();
 
     const rollTree = new RollNode([numAttackers, numDefenders]);
     const treeLeafOdds = rollTree.getOdds(modifiers);
-    // console.log(treeLeafOdds);
-
-    // console.log('Keys:', Object.keys(treeLeafOdds));
-    // console.log('Values:', Object.values(treeLeafOdds));
-
-    // const oddsOutcomes = treeLeafOdds.map((obj) => obj.outcome);
-    // const oddsProbabilities = treeLeafOdds.map((obj) => obj.probability);
 
     const attackWins = [];
     const attackLoses = [];
 
     for (let i = 0; i < treeLeafOdds.length; i++) {
-      // const oddsObject = {};
-      const outcome = treeLeafOdds[i].outcome;
-      console.log('outcome:', outcome);
       if (treeLeafOdds[i].outcome[0] !== '0') { 
         attackWins.push(treeLeafOdds[i]);
       } else {
         attackLoses.push(treeLeafOdds[i]);
       }
     }
-    // console.log(attackWins);
-    // console.log(attackLoses);
 
     setOddsAttackWins(attackWins);
     setOddsAttackLoses(attackLoses);
@@ -51,22 +37,26 @@ const App = () => {
   const resetForm = () => {
     setNumAttackers(0);
     setNumDefenders(0);
+    setModifiers({ defender: "" });
   };
   
   const displayOutcomes = (outcomeArray) => {
     const displayedOutcomes = outcomeArray.map((outcome) => {
       return (
-        <div key={outcome.outcome}>
+        <div className="outcomesTable__outcomeItem" key={outcome.outcome}>
           <p>{outcome.outcome}</p>
           <p>{((outcome.probability)*100).toFixed(2)}</p>
         </div>
       );
     });
 
-    return displayedOutcomes;
+    return (
+      <ul className="outcomesTable__outcomesList">
+        {displayedOutcomes}
+      </ul>
+    );
   };
 
-  // TODO: redo leaf probabilities first to have easier object to work with
   const displayTotalOutcome = (outcomeArray) => {
     if (outcomeArray.length <= 0) { return 0; }
     const probabilitiesSum = outcomeArray.map((outcome) => outcome.probability)
@@ -75,21 +65,47 @@ const App = () => {
     return probabilitiesSum;
   };
 
+  // Only works for defenders for now
+  const modifyModifiers = (modifier) => {
+    setModifiers({ defender: modifier });
+  };
+
+  // TODO: calculate median/average outcome
+
   return (
     <div className="App">
-      <form onSubmit={(event) => calculateOdds(event)}>
-        <label htmlFor="numAttackers">Attackers</label>
-        <input
-          type="number" name="numAttackers" min="0" max="99"
-          value={numAttackers}
-          onChange={(event) => setNumAttackers(event.target.value)}
-        />
-        <label htmlFor="numDefenders">Defenders</label>
-        <input
-          type="number" name="numDefenders" min="0" max="99"
-          value={numDefenders}
-          onChange={(event) => setNumDefenders(event.target.value)}
-        />
+      <form className="inputsForm" onSubmit={(event) => calculateOdds(event)}>
+
+        <div className="inputsForm__troopsCount">
+          <h2>Troops</h2>
+          <div className="inputsForm__input">
+            <label htmlFor="numAttackers">Attackers</label>
+            <input
+              type="number" name="numAttackers" min="1" max="99"
+              value={numAttackers} placeholder="0" required
+              onChange={(event) => setNumAttackers(event.target.value)}
+            />
+
+            <label htmlFor="numDefenders">Defenders</label>
+            <input
+              type="number" name="numDefenders" min="1" max="99"
+              value={numDefenders} placeholder="0" required
+              onChange={(event) => setNumDefenders(event.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="inputsForm__modifiers">
+          <h2>Modifiers</h2>
+          <label htmlFor="defenderModifiers">Defenders Scar:</label>
+          <select onChange={(event) => modifyModifiers(event.target.value)}>
+            <option value="n/a">none</option>
+            <option value="bunker">Bunker</option>
+            <option value="ammoShortage">Ammo Shortage</option>
+          </select>
+        
+        </div>
+
         <button type="reset" onClick={() => resetForm()}>Reset</button>
         <button type="submit">Calculate Odds</button>
       </form>
